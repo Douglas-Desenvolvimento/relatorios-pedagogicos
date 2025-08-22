9be4913c101804276ee211ba04459e28b252099b
Douglas-Desenvolvimento <douglas.silva.it@gmail.com>
2025-08-22 17:19:32 -0300

    ajuste para vercel 3

--- relatorios-pedagogicos/src/app/api/materias/[id]/route.ts
+++ relatorios-pedagogicos/src/app/api/materias/[id]/route.ts
@@ -1,9 +1,10 @@
 // src/app/api/materias/[id]/route.ts
 import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
